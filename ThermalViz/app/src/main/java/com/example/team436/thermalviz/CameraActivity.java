57653e1b2f3a45c011171214fcadbdf669f0d09e
jp1022 <jep1911@umd.edu>
2015-11-09 15:57:47 -0500

    main layout ready
    Main menu with corresponding styles ready. Function wrappers ready for
    camera and gallery functionalities.

---
+++ ThermalViz/app/src/main/java/com/example/team436/thermalviz/CameraActivity.java
@@ -0,0 +1,21 @@
+package com.example.team436.thermalviz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by John on 11/7/15.
+ */
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_layout);
+  //      Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+    }
+}
