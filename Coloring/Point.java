fd097b4d2f9b8b2a96c2b775e84fd29e9309420c
vwong1 <vwong1@terpmail.umd.edu>
2015-12-03 10:31:21 -0500

    Coloring Change
    Coloring Change

---
+++ Coloring/Point.java
@@ -0,0 +1,16 @@
+package com.example.joe.opencvcameratest;
+
+//Object class for use with Coloring.java
+public class Point {
+    public int x;
+    public int y;
+
+    public float temperature;
+
+    public Point(int x, int y, float temperature){
+        this.x = x;
+        this.y = y;
+        this.temperature = temperature;
+    }
+
+}
